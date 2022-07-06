e942c8816cc13d078f995c788d42a43a1d2023bb
Biniyamseid <biniyamseid3@gmail.com>
2022-07-05 19:45:53 -0700

    week two

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(){
+	printf("a");
+	printf("a");
+}
+
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
